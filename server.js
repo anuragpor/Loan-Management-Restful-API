a8f5567324d691df367f8fae3077ec54f52b1acc
Anurag Porwal <55937254+anuragpor@users.noreply.github.com>
2021-04-20 17:46:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,28 +3,15 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE_LOCAL;
 mongoose.connect(DB, {  // these are some option to deal with deprecation warning
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
-}).then(con => { // it gives us a promise which we have to take care using then method
-  // console.log(con.connection);
+}).then(con => { 
   console.log("DB connection established");
 });
 
-
-
-// const testTour = new Tour({
-//   name: 'The Park Campus 2', //Our validators will make sure of that no duplicate name occur
-//   price: 200
-// });
-
-// testTour.save().then(doc => {  // it returns a promise which we handle using then and catch method
-//   console.log(doc);
-// }).catch(err => {
-//   console.log("Some error read the error message", err);
-// });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
