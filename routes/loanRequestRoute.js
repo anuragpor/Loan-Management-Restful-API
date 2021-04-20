68caa6e060d6c63031a6caf9fb17bf9f9812d4a6
Anurag Porwal <55937254+anuragpor@users.noreply.github.com>
2021-04-20 17:43:10 +0530

    Update loanRequestRoute.js

--- routes/loanRequestRoute.js
+++ routes/loanRequestRoute.js
@@ -4,8 +4,6 @@ const router = express.Router(); // this is called mounting the routers.
 
 const loanController = require('../controllers/loanController'); 
 
-// router.route('/').get(tourController.getAllTours).post(tourController.checkBody,tourController.createTour);
-
 router.route('/').get(loanController.getAllLoans).post(loanController.createLoan);
 router.route('/:id').delete(loanController.deleteLoan).patch(loanController.updateLoan).get(loanController.getLoan);
 
